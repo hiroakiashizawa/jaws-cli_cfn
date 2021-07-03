86d290fb6a905ae133704996fc7f29fa9ee99ffe
hiroakiashizawa <jo.ultim.layco@gmail.com>
2021-07-03 11:07:34 +0900

    commit main

--- create_cloud9.sh
+++ create_cloud9.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ## CloudFormation実行
-
+aws cloudformation create-stack \
+--stack-name handson-cloud9 \
+--template-body file://create_cloud9_env.yml \
+--capabilities CAPABILITY_NAMED_IAM
 
 ## Instance Profile関連付け
 ### 変数設定
@@ -14,11 +17,24 @@ ARRAY_EC2_INSTANCE_IDS=$( \
     --query Reservations[].Instances[].InstanceId \
     --output text \
 ) \
+  && echo ${ARRAY_EC2_INSTANCE_IDS}
+
 EC2_INSTANCE_ID=$( \
   echo ${ARRAY_EC2_INSTANCE_IDS} \
     | sed 's/ .*$//' \
 ) \
-STRING_EC2_INSTANCE_PROFILE="Arn=${IAM_INSTANCE_PROFILE_ARN},Name=${IAM_INSTANCE_PROFILE_NAME}"
+  && echo ${EC2_INSTANCE_ID}
+
+IAM_INSTANCE_PROFILE_ARN=$( \
+  aws iam get-instance-profile \
+    --instance-profile-name ${IAM_INSTANCE_PROFILE_NAME} \
+    --query 'InstanceProfile.Arn' \
+    --output text \
+) \
+  && echo ${IAM_INSTANCE_PROFILE_ARN}
+
+STRING_EC2_INSTANCE_PROFILE="Arn=${IAM_INSTANCE_PROFILE_ARN},Name=${IAM_INSTANCE_PROFILE_NAME}" \
+  && echo ${STRING_EC2_INSTANCE_PROFILE}
 
 ## 関連付け実行
 aws ec2 associate-iam-instance-profile \
